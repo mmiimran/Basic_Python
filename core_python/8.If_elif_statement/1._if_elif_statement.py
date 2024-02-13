798d9db72e796461ae985ea9ebe981ad3e5f9c88
mmiimran <go2imran6@gmail.com>
2024-02-14 03:11:22 +0600

    if elif statement

---
+++ core_python/8.If_elif_statement/1._if_elif_statement.py
@@ -0,0 +1,30 @@
+# If elif Statement
+a = 20
+b = 30
+
+# Checking if 'a' is greater than 'b'
+if a > b:
+    print("a is greater than b")
+# If 'a' is not greater than 'b', checking if 'a' is equal to 'b'
+elif a == b:
+    print("a and b are equal")
+# If 'a' is neither greater than 'b' nor equal to 'b', it must be less than 'b'
+elif a < b:
+    print("a is less than b")
+
+
+# If elif with User Input
+# Prompting the user to enter the value of 'a' and storing it in variable 'a'
+a = int(input("Enter Value of A: "))
+# Prompting the user to enter the value of 'b' and storing it in variable 'b'
+b = int(input("Enter Value of B: "))
+
+# Checking if 'a' is greater than 'b'
+if a > b:
+    print("a is greater than b")
+# If 'a' is not greater than 'b', checking if 'a' is equal to 'b'
+elif a == b:
+    print("a and b are equal")
+# If 'a' is neither greater than 'b' nor equal to 'b', it must be less than 'b'
+elif a < b:
+    print("a is less than b")
