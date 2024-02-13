f4aa8fa08a442f862f720670626ce07f91093717
mmiimran <go2imran6@gmail.com>
2024-02-14 04:39:02 +0600

    brak and continue

---
+++ core_python/13.Break_and_Continue/1._break_and_continue.py
@@ -0,0 +1,28 @@
+# Break Statement
+# Iterating over the range from 0 to 9
+for i in range(10):
+    # Checking if the current value of 'i' is equal to 5
+    if i == 5:
+        # Exiting the loop when 'i' equals 5
+        break
+    # Printing the current value of 'i'
+    print(i)
+
+# Printing a message after the loop finishes
+print("Rest of the Code")
+
+# Printing a line separator
+print("****************")
+
+# Continue Statement
+# Iterating over the range from 0 to 9
+for i in range(10):
+    # Checking if the current value of 'i' is equal to 5
+    if i == 5:
+        # Skipping the current iteration when 'i' equals 5
+        continue
+    # Printing the current value of 'i' for all other values of 'i'
+    print(i)
+
+# Printing a message after the loop finishes
+print("Rest of the Code")
