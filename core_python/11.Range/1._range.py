9beed97eae22e67b9ff6e241ffe30609f0eeda4c
mmiimran <go2imran6@gmail.com>
2024-02-14 04:18:56 +0600

    range

---
+++ core_python/11.Range/1._range.py
@@ -0,0 +1,54 @@
+# Range Function
+# Printing a message indicating the usage of range with stop value
+print("Range with stop")
+# Creating a range object with stop value 5
+a = range(5)
+# Printing elements of the range individually
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+# Printing a message indicating the usage of range with start and stop values
+print("Range with start, stop")
+# Creating a range object with start value 1 and stop value 5
+b = range(1, 5)
+# Printing elements of the range individually
+print(b[0])
+print(b[1])
+print(b[2])
+print(b[3])
+
+# Printing a message indicating the usage of range with start, stop, and step values
+print("Range with start, stop, step")
+# Creating a range object with start value 1, stop value 10, and step value 2
+c = range(1, 10, 2)
+# Printing elements of the range individually
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
+print(c[4])
+
+# Printing a message indicating the usage of negative range with start, stop, and step values
+print("Negative Range with start, stop, step")
+# Creating a range object with start value -1, stop value -10, and step value -2
+n = range(-1, -10, -2)
+# Printing elements of the range individually
+print(n[0])
+print(n[1])
+print(n[2])
+print(n[3])
+print(n[4])
+
+# Printing a message indicating the usage of reverse range with start, stop, and step values
+print("Reverse Range with start, stop, step")
+# Creating a range object with start value 5, stop value 0, and step value -1
+r = range(5, 0, -1)
+# Printing elements of the range individually
+print(r[0])
+print(r[1])
+print(r[2])
+print(r[3])
+print(r[4])
