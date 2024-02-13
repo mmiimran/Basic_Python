f2a142259cf487ba6e2ba6e76283ba61e8ed4b04
mmiimran <go2imran6@gmail.com>
2024-02-14 04:43:44 +0600

    pass

---
+++ core_python/14.Pass/1._pass.py
@@ -0,0 +1,30 @@
+# Pass Statement
+# Checking if the condition 5 < 2 is true, which it isn't
+if 5 < 2:
+    # 'pass' does nothing and allows the program to continue
+    pass
+else:
+    # Printing a message as the condition is not met
+    print("Else Part Do Something")
+
+# Printing a message after the if-else block
+print("Rest of the Code")
+
+# Printing a line separator
+print("*****************")
+
+# Initializing variable 'i' to 1
+i = 1
+# Looping while 'i' is less than or equal to 10
+while i <= 10:
+    # Checking if 'i' is equal to 5
+    if i == 5:
+        # 'pass' does nothing and allows the loop to continue
+        pass
+    # Printing the value of 'i' regardless of the condition
+    print(i)
+    # Incrementing 'i' by 1
+    i += 1
+
+# Printing a message after the loop finishes
+print("Rest of the Code")
