0512bf9fea1b287f5c2a3a3d07aa36c598b662e6
mmiimran <go2imran6@gmail.com>
2024-02-14 02:28:55 +0600

    escape sequences

---
+++ core_python/4.Escape_Sequence_in_python/1._escape_sequence.py
@@ -0,0 +1,14 @@
+# Printing a simple string
+print("Welcome to Code2learn")
+
+# Printing a string with a newline character
+print("Welcome to\nCode2learn")
+
+# Printing a string with a tab character
+print("Welcome to\tCode2learn")
+
+# Printing a string with double quotes using escape sequence
+print("Welcome to \"Code2learn\"")
+
+# Printing a string with single quotes using escape sequence
+print("Welcome to \'Code2learn\'")
