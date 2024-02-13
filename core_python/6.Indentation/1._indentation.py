58c2c6ebb64176dca015e269ac0f109e6f03dd3b
mmiimran <go2imran6@gmail.com>
2024-02-14 02:54:36 +0600

    indentation

---
+++ core_python/6.Indentation/1._indentation.py
@@ -0,0 +1,13 @@
+# Checking if 7 is greater than 2
+if 7 > 2:
+    # Printing "Greater" if the condition is true
+    print("Greater")
+    # Printing "7 is greater than 2"
+    print("7 is greater than 2")
+    # Checking if 8 is greater than 2
+    if 8 > 2:
+        # Printing "8 is Greater than 2" if the condition is true
+        print("8 is Greater than 2")
+
+# Printing "Rest of The Code" outside of the if block
+print("Rest of The Code")
