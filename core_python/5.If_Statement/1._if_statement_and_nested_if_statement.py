5ccd4dc4ea7669dbe2c1e2a8d08e34b72deaf3ee
mmiimran <go2imran6@gmail.com>
2024-02-14 02:43:05 +0600

    if statement

---
+++ core_python/5.If_Statement/1._if_statement_and_nested_if_statement.py
@@ -0,0 +1,42 @@
+# If statement
+# Check if 7 is greater than 2
+if 7 > 2:
+    print("Greater")
+
+# If Statement in Single Line
+# Check if 7 is greater than 2 and print "Greater" if true
+if 7 > 2: print("Greater")
+
+# If Statement with Rest of the Code
+# Check if 7 is greater than 2 and print "Greater", then print "Rest of The Code"
+if 7 > 2:
+    print("Greater")
+print("Rest of The Code")
+
+# If Statement with Group of Statement
+# Check if 7 is greater than 2 and print "Greater", 
+# then print "5 is greater than 2", and finally print "Rest of The Code"
+if 7 > 2:
+    print("Greater")
+    print("5 is greater than 2")
+print("Rest of The Code")
+
+# Nested If Statement
+# Check if 7 is greater than 2 and print "Greater",
+# then check if 6 is greater than 2 and print "6 is Greater than 2",
+# and finally print "Rest of The Code"
+if 7 > 2:
+    print("Greater")
+    print("5 is greater than 2")
+    if 6 > 2:
+        print("6 is Greater than 2")
+print("Rest of The Code")
+
+# If statement with Logical Operator
+# Check if 5 is greater than 2 and 7 is less than 3,
+# if true, print "If statement with Logical Operator" and "Statement 2",
+# then print "Rest of the Code"
+if 5 > 2 and 7 < 3:
+    print("If statement with Logical Operator")
+    print("Statement 2")
+print("Rest of the Code")
---
+++ core_python/5.If_Statement/2._if_statement_with_user_input.py
@@ -0,0 +1,7 @@
+# Getting user input for a number greater than 4
+a = int(input("Enter Number Greater than 4: "))
+
+# Checking if the entered number is greater than 4
+if a > 4:
+    # If condition is true, print the entered number
+    print("You have Entered:", a)
