567a30024cf9922edb6830315a8bf35ad1f3286d
mmiimran <go2imran6@gmail.com>
2024-03-10 17:47:52 +0600

    1D logspace function

---
+++ core_python/18.Numpy_1D_logspace_function/1_log_space_function.py
@@ -0,0 +1,35 @@
+# 1D Array using logspace Function Numpy
+from numpy import *
+
+# Create a 1D array using logspace function
+a = logspace(1, 3, 5)
+
+# Print individual elements of the array
+print("**** Accessing Individual Elements ****")
+print(a[0])  # Accessing the first element
+print(a[1])  # Accessing the second element
+print(a[2])  # Accessing the third element
+print(a[3])  # Accessing the fourth element
+print(a[4])  # Accessing the fifth element
+print()
+
+# Accessing elements using a for loop
+print("**** Accessing by For Loop ****")
+for el in a:
+    print(el)
+print()
+
+# Accessing elements using a for loop with index
+print("**** Accessing by For Loop with Index ****")    
+n = len(a)
+for i in range(n):
+    print('index',i,'=',a[i])
+print()
+
+# Accessing elements using a while loop with index
+print("**** Accessing by While Loop ****")    
+n = len(a)
+i = 0
+while i < n:
+    print('index',i,'=',a[i])
+    i += 1
