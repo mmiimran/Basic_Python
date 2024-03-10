9bf7abe0dd4105a014df9f93f0e7f6cd4833f9cd
mmiimran <go2imran6@gmail.com>
2024-03-10 20:43:39 +0600

    1D ones function

---
+++ core_python/21.Numpy_1D_ones_function/1_ones_function.py
@@ -0,0 +1,36 @@
+# 1D Array using ones Function Numpy
+from numpy import *
+
+# Create a 1D array filled with ones using ones function
+a = ones(5)
+# a = ones(5, dtype=int)  # Use this line to create an array of ones with integer data type
+
+# Print individual elements of the array
+print("**** Accessing Individual Elements ****")
+print(a[0])  # Accessing the first element
+print(a[1])  # Accessing the second element
+print(a[2])  # Accessing the third element
+print(a[3])  # Accessing the fourth element
+print(a[4])  # Accessing the fifth element
+print()
+
+# Accessing elements using a for loop
+print("**** Accessing by For Loop ****")
+for el in a:
+    print(el)
+print()
+
+# Accessing elements using a for loop with index
+print("**** Accessing by For Loop with Index ****")    
+n = len(a)
+for i in range(n):
+    print('index',i,'=',a[i])
+print()
+
+# Accessing elements using a while loop with index
+print("**** Accessing by While Loop ****")    
+n = len(a)
+i = 0
+while i < n:
+    print('index',i,'=',a[i])
+    i += 1
