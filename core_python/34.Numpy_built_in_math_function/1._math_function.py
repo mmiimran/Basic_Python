b308d606dcbaa71a1e2aa981d1fee0acdb642330
mmiimran <go2imran6@gmail.com>
2024-03-10 22:31:31 +0600

    numpy built in math function

---
+++ core_python/34.Numpy_built_in_math_function/1._math_function.py
@@ -0,0 +1,25 @@
+from numpy import *
+
+# Creating numpy arrays 'a' and 'b'
+a = array([25, 81, 49, 64])
+b = array([[4, 9, 16, 25], [36, 16, 4, 49]])
+
+# Printing a blank line for better readability
+print()
+
+# sum() Function
+# Calculating the sum of elements in arrays 'a' and 'b'
+print("a:", sum(a))
+print("b:", sum(b))
+print()
+
+# prod() Function
+# Calculating the product of elements in arrays 'a' and 'b'
+print("a:", prod(a))
+print("b:", prod(b))
+print()
+
+# sqrt() Function
+# Calculating the square root of elements in arrays 'a' and 'b'
+print("a:", sqrt(a))
+print("b:", sqrt(b))
