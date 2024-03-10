6e34f7d5ff94f53eb1c5849e37e0e65972ea3706
mmiimran <go2imran6@gmail.com>
2024-03-10 22:05:32 +0600

    reshape and flatten function

---
+++ core_python/30.Reshape_and_flatten_function/2._flatten_function.py
@@ -0,0 +1,13 @@
+# flatten() Function
+# 2D to 1D
+from numpy import *
+
+# Creating a 2D numpy array 'e'
+e = array([[1, 2, 3], [4, 5, 6]])
+
+# Using flatten() function to convert the 2D array 'e' to a 1D array 'f'
+f = e.flatten()
+
+# Printing the original 2D array 'e' and the flattened 1D array 'f'
+print(e)
+print(f)
