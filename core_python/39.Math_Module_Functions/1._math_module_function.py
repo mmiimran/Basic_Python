c13a058a3e71fd1f18c9cae0e3d53d6c2770e7c2
mmiimran <go2imran6@gmail.com>
2024-03-26 22:24:13 +0600

    math module function

---
+++ core_python/39.Math_Module_Functions/1._math_module_function.py
@@ -0,0 +1,11 @@
+# Importing specific functions from the 'math' module
+
+# Importing the 'floor', 'ceil', 'sqrt', 'factorial', and 'pow' functions from the 'math' module
+from math import *
+
+# Using the imported functions
+print(floor(4.5))       # Prints the floor value of 4.5
+print(ceil(4.5))        # Prints the ceiling value of 4.5
+print(sqrt(49))         # Prints the square root of 49
+print(factorial(5))     # Prints the factorial of 5
+print(pow(5, 2))        # Prints 5 raised to the power of 2
